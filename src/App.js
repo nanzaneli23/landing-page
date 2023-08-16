7fa29ff4a446fb8fbb7b11e8844fa0249c3bf4bd
nanzaneli23 <renanzaneli01@gmail.com>
2023-08-16 14:58:15 -0300

    Modelo do react pronto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import Contador from "./components/Contador";
+import ExibeNota from "./components/ExibeNota";
+import { useState } from "react";
+import BoasVindas from "./components/BoasVindas";
+
+function App(props) {
+
+  const [ contador , setContador] = useState(0);
+  
+  function Aumento()
+  {
+    setContador( contador + 1);
+  }
+  function Diminuir()
+  {
+    setContador( contador - 1);
+  }
 
 
-function App() {
+  let nota= props.nota;
+  let aprovado = false;
+  let reprovado = false;
+  if(nota>5){
+    aprovado = true;
+  } else{
+    reprovado = true;
+  }
+
+ 
+  
   return (
    <>
-   
+   { aprovado && 
+   <ExibeNota mensagem="Aprovado" nota={props.nota} />
+}
+
+   { reprovado && 
+     <ExibeNota mensagem="Reprovado" nota={props.nota} />
+}
+<Contador/>
+<BoasVindas/>
+
+
+
+  
    </>
   );
 }
---
+++ src/components/ExibeNota.jsx
@@ -0,0 +1,16 @@
+function ExibeNota(props)
+{
+    function ExibeNota()
+    {
+        alert( "Sua média geral foi:"  + props.nota);
+    }
+    return (
+    
+        <div>
+          <h1>{props.mensagem}</h1>
+          <button onClick={ExibeNota}>Veja sua nota</button>
+        </div>
+    )
+     
+}
+export default ExibeNota;
